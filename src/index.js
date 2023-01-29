09293e3f602662b5a2783b886933774c30ddbd3e
Sibsankar-de <sibsankarde910@gmail.com>
2023-01-29 18:49:26 +0530

    last commit for router

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <BrowserRouter > */}
-    <HashRouter basename="/">
+    <HashRouter>
       <App />
     </HashRouter>
     {/* </BrowserRouter> */}
