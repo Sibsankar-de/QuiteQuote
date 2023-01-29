80facdcfcf06054b6df0f05e349d37127417ffcb
Sibsankar-de <sibsankarde910@gmail.com>
2023-01-29 18:43:51 +0530

    commit to change-

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
-      <HashRouter>
+    <BrowserRouter >
+      <HashRouter basename="/">
         <App />
       </HashRouter>
     </BrowserRouter>
